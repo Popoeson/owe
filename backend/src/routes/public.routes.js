b3e9d50f3b709701054fa1f7c3f0201c9f09a381
Samuel Popoola <samuelpopoola323@gmail.com>
2026-09-26 04:50:29 +0100

    Update public.routes.js

--- backend/src/routes/public.routes.js
+++ backend/src/routes/public.routes.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const { initiateRegistration, getPaymentStatus, manualVerify, getPaymentDetails } = require('../controllers/paymentController');
+const { getStatBoard, initiateVote } = require('../controllers/voteController');
 
 router.post('/register/initiate', initiateRegistration);
 router.get('/payments/:reference/status', getPaymentStatus);
 router.post('/payments/:reference/verify', manualVerify);
 router.get('/payments/:reference', getPaymentDetails);
+router.get('/stat-board', getStatBoard);
+router.post('/vote/initiate', initiateVote);
 
 module.exports = router;
