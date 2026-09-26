9f8d6ccc976a761a2a75f6056e6ed63ebc7774c9
Samuel Popoola <samuelpopoola323@gmail.com>
2026-09-26 04:00:39 +0100

    Update admin.routes.js

--- backend/src/routes/admin.routes.js
+++ backend/src/routes/admin.routes.js
@@ -4,6 +4,7 @@ const requireAdmin = require('../middleware/requireAdmin');
 const { login, logout, setup, session } = require('../controllers/authController');
 const { getStats } = require('../controllers/dashboardController');
 const performers = require('../controllers/performerAdminController');
+const { listPayments } = require('../controllers/paymentAdminController');
 
 router.post('/login', login);
 router.post('/logout', logout);
@@ -19,5 +20,7 @@ router.patch('/performers/:id/reject', requireAdmin, performers.rejectPerformer)
 router.patch('/performers/:id/deactivate', requireAdmin, performers.deactivatePerformer);
 router.patch('/performers/:id/reactivate', requireAdmin, performers.reactivatePerformer);
 router.delete('/performers/:id', requireAdmin, performers.hardDeletePerformer);
+router.get('/payments', requireAdmin, listPayments);
+
 
 module.exports = router;
